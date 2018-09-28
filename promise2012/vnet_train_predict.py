5bd9005c2337a5808eb2292fe788d6726ee16b90
junqiangchen <1207173174@qq.com>
2018-09-28 16:39:34 +0800

    add how to use model transform

--- promise2012/vnet_train_predict.py
+++ promise2012/vnet_train_predict.py
@@ -1,7 +1,8 @@
-from Vnet2d.vnet_model import Vnet2dModule
+from promise2012.Vnet2d.vnet_model import Vnet2dModule
 import numpy as np
 import pandas as pd
 import cv2
+from promise2012.Vnet2d.util import convertMetaModelToPbModel
 
 
 def train():
@@ -9,8 +10,8 @@ def train():
     Preprocessing for dataset
     '''
     # Read  data set (Train data from CSV file)
-    csvmaskdata = pd.read_csv('PROMISE2012Mask.csv')
-    csvimagedata = pd.read_csv('PROMISE2012Image.csv')
+    csvmaskdata = pd.read_csv('promise2012vnet2dMask.csv')
+    csvimagedata = pd.read_csv('promise2012vnet2dimage.csv')
     maskdata = csvmaskdata.iloc[:, :].values
     imagedata = csvimagedata.iloc[:, :].values
     # shuffle imagedata and maskdata together
@@ -20,15 +21,20 @@ def train():
     maskdata = maskdata[perm]
 
     unet2d = Vnet2dModule(512, 512, channels=1, costname="dice coefficient")
-    unet2d.train(imagedata, maskdata, "model\\Vnet2dModule.pd", "log\\", 0.001, 0.5, 100000, 4)
+    unet2d.train(imagedata, maskdata, "model\\Vnet2dModule.pd", "log\\", 0.001, 0.5, 2, 4)
 
 
 def predict():
-	image = cv2.imread("1.bmp",0)
-    unet2d = Vnet2dModule(512, 512, 1)
-    predictvalue = unet2d.prediction("model\\Vnet2dModule.pd", image)
-	cv2.imwrite("mask.bmp", predictvalue)
+    image = cv2.imread("1.bmp", 0)
+    unet2d = Vnet2dModule(512, 512, 1, model_path="model\\Vnet2dModule.pd")
+    predictvalue = unet2d.prediction(image)
+    cv2.imwrite("mask.bmp", predictvalue)
 
 
-train()
+def meta2pd():
+    convertMetaModelToPbModel(meta_model="model\\Vnet2dModule.pd", pb_model="model")
+
+
+# train()
 # predict()
+meta2pd()
